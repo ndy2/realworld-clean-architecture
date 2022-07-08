11f5507eb10cb4027f2b4743e1d9672c73274aec
ndy2 <emrdbs12@gmail.com>
2022-07-09 00:39:11 +0900

    feat: 인증매니저, 패스워드 인코더 빈 설정 추가

--- src/main/java/com/deukyun/realworld/configuration/SecurityConfiguration.java
+++ src/main/java/com/deukyun/realworld/configuration/SecurityConfiguration.java
@@ -1,8 +1,12 @@
 package com.deukyun.realworld.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
@@ -31,5 +35,18 @@ public class SecurityConfiguration {
 
                 .sessionManagement().sessionCreationPolicy(STATELESS)
                 .and().build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
     }
 }
