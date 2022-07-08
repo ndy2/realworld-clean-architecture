76d8e642f4098e418f513646a87e947cc466b36c
ndy2 <emrdbs12@gmail.com>
2022-07-09 00:34:02 +0900

    chore: @Query 어노테이션 추가

---
+++ src/main/java/com/deukyun/realworld/common/component/Query.java
@@ -0,0 +1,24 @@
+
+package com.deukyun.realworld.common.component;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface Query {
+
+  /**
+   * The value may indicate a suggestion for a logical component name,
+   * to be turned into a Spring bean in case of an autodetected component.
+   *
+   * @return the suggested component name, if any (or empty String otherwise)
+   */
+  @AliasFor(annotation = Component.class)
+  String value() default "";
+
+}
