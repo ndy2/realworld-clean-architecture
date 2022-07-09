23b9e315363f93058f9f608f4de2594fd3f59876
ndy2 <emrdbs12@gmail.com>
2022-07-09 19:47:20 +0900

    feat: 사용자 업데이트 요청, 응답

--- src/main/java/com/deukyun/realworld/user/adapter/in/web/EditUserRequest.java
+++ src/main/java/com/deukyun/realworld/user/adapter/in/web/EditUserRequest.java
@@ -1,4 +1,42 @@
 package com.deukyun.realworld.user.adapter.in.web;
 
-public class EditUserRequest {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+class EditUserRequest {
+
+    @JsonProperty("user")
+    private Request request;
+
+    public String getEmail() {
+        return request.email;
+    }
+
+    public String getPassword() {
+        return request.password;
+    }
+
+    public String getUsername() {
+        return request.username;
+    }
+
+    public String getImage() {
+        return request.image;
+    }
+
+    public String getBio() {
+        return request.bio;
+    }
+
+    @Getter
+    private static class Request {
+
+        private String email;
+        private String password;
+        private String username;
+        private String image;
+
+        private String bio;
+    }
+
 }
