42a52240bdc86659b6c42aeebdab5e9fda6f8e44
ndy2 <emrdbs12@gmail.com>
2022-07-11 00:25:40 +0900

    feat: 언페이보릿 서비

--- src/main/java/com/deukyun/realworld/favorite/application/service/FavoriteArticleService.java
+++ src/main/java/com/deukyun/realworld/favorite/application/service/FavoriteArticleService.java
@@ -16,6 +16,8 @@ import com.deukyun.realworld.profile.application.port.out.FindProfileIdByUserIdP
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 import static com.google.common.base.Preconditions.checkArgument;
 
 @RequiredArgsConstructor
@@ -79,6 +81,47 @@ class FavoriteArticleService implements
     @Transactional
     @Override
     public FavoriteArticleResult unfavorite(long userId, String slug) {
-        return null;
+        // 1. 슬러그로 아티클 조회
+        FindArticleResult article = findArticleBySlugPort.findArticleBySlug(slug);
+        long articleId = article.getId();
+
+        // 2. 언페이보릿 할 수 있는지 확인하고 페이보릿 제거
+        long favoriteId = checkUnfFavoritable(userId, articleId);
+        deleteFavoritePort.deleteById(favoriteId);
+
+        // 3. 팔로우 여부를 확인 하기 위해 자신의 프로필 아이디 조회
+        long userProfileId = findProfileIdByUserIdPort.findProfileIdByUserId(userId);
+
+        // 4. 팔로우 여부 확인
+        FindAuthorResult author = article.getAuthor();
+        boolean isFollow = checkFollowPort.checkFollow(userProfileId, author.getId()).isPresent();
+
+        // 5. 데이터 반환
+        return new FavoriteArticleResult(
+                article.getSlug(),
+                article.getTitle(),
+                article.getDescription(),
+                article.getBody(),
+                article.getTagList(),
+                article.getCreatedAt(),
+                article.getUpdatedAt(),
+                true,
+                1,
+                new FavoriteAuthorResult(
+                        author.getUsername(),
+                        author.getBio(),
+                        author.getImage(),
+                        isFollow
+                )
+        );
+    }
+
+    private long checkUnfFavoritable(long userId, long articleId) {
+        Optional<Long> idIfPresent = checkFavoritePort.checkFavorite(userId, articleId);
+
+        boolean isAlreadyFavorite = idIfPresent.isPresent();
+
+        checkArgument(isAlreadyFavorite, "페이보릿 상태가 아닙니다");
+        return idIfPresent.get();
     }
 }
